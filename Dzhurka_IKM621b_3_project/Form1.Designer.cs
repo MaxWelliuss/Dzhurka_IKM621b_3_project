bcfb15a765645a623cdaaf0d59a52085259ab537
Максим Джурка <Maksym.Dzhurka@cit.khpi.edu.ua>
2024-04-03 15:46:40 +0300

    Finish task number 1

--- Dzhurka_IKM621b_3_project/Form1.Designer.cs
+++ Dzhurka_IKM621b_3_project/Form1.Designer.cs
@@ -30,12 +30,68 @@ namespace Dzhurka_IKM621b_3_project
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tbInput1 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbInput1
+            // 
+            this.tbInput1.AutoSize = true;
+            this.tbInput1.Location = new System.Drawing.Point(310, 51);
+            this.tbInput1.Name = "tbInput1";
+            this.tbInput1.Size = new System.Drawing.Size(46, 17);
+            this.tbInput1.TabIndex = 0;
+            this.tbInput1.Text = "label1";
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(295, 267);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Enabled = false;
+            this.tbInput.Location = new System.Drawing.Point(279, 161);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 22);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.TextChanged += new System.EventHandler(this.tbInput_TextChanged);
+            this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.tbInput1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label tbInput1;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Timer tClock;
     }
 }
 
